9b5b368b4fe93d811b15b3390f4689a69ce83d6c
Wutche <wutchechikaome75@gmail.com>
2023-10-29 20:49:31 +0100

    user can now be able to upload their personal logo on the shirt ):

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -4,7 +4,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import config from "../config/config";
 import state from "../store";
 import { download } from "../assets";
-import { downloadCanvasToImage } from "../config/helpers";
+import { downloadCanvasToImage, reader } from "../config/helpers";
 import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
 import { fadeAnimation, slideAnimation } from "../config/motion";
 import {
@@ -39,12 +39,50 @@ const Customizer = () => {
 			case "colorpicker":
 				return <ColorPicker />;
 			case "filepicker":
-				return <FilePicker />;
+				return <FilePicker file={file} setFile={setFile} readFile={readFile} />;
 			case "aipicker":
 				return <Aipicker />;
 			default:
 				return null;
 		}
+	};
+
+	const handleDecals = (type, result) => {
+		const decalType = DecalTypes[type];
+		state[decalType.stateProperty] = result;
+
+		if (!activeFilterTab[decalType.filterTab]) {
+			handleActiveFilterTab(decalType.filterTab);
+		}
+	};
+
+	const handleActiveFilterTab = (tabname) => {
+		switch (tabname) {
+			case "logoShirt":
+				state.isLogoTexture = !activeFilterTab[tabname];
+				break;
+			case "sylishShirt":
+				state.isFullTexture = !activeFilterTab[tabname];
+
+			default:
+				state.isLogoTexture = true;
+				state.isFullTexture = false;
+		}
+
+		// after setting the state, activeFilterTab is updated
+		setactiveFilterTab((prevState) => {
+			return {
+				...prevState,
+				[tabname]: !prevState[tabname],
+			};
+		});
+	};
+
+	const readFile = (type) => {
+		reader(file).then((result) => {
+			handleDecals(type, result);
+			setactiveEditorTab("");
+		});
 	};
 	return (
 		<AnimatePresence>
@@ -57,7 +95,11 @@ const Customizer = () => {
 						<div className="flex items-center min-h-screen">
 							<div className="editortabs-container tabs">
 								{EditorTabs.map((tab) => (
-									<Tab key={tab.name} tab={tab} handleClick={() => setactiveEditorTab(tab.name)} />
+									<Tab
+										key={tab.name}
+										tab={tab}
+										handleClick={() => setactiveEditorTab(tab.name)}
+									/>
 								))}
 								{generateTabContent()}
 							</div>
@@ -81,9 +123,9 @@ const Customizer = () => {
 							<Tab
 								key={tab.name}
 								isFilter
-								isActive=""
+								isActive={activeFilterTab[tab.name]}
 								tab={tab}
-								handleClick={() => {}}
+								handleClick={() => handleActiveFilterTab(tab.name)}
 							/>
 						))}
 					</motion.div>
